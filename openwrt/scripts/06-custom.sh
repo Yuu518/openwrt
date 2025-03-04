d870099baf1e9223fc05ee707e06051998705098
Yuu <yuumi2836@gmail.com>
2025-03-04 10:45:30 +0800

    Custom

--- openwrt/scripts/06-custom.sh
+++ openwrt/scripts/06-custom.sh
@@ -14,3 +14,6 @@ git clone https://$github/sbwml/packages_utils_lrzsz package/new/lrzsz
 # timedreboot
 rm -rf package/new/extd/luci-app-timedreboot
 git clone https://$github/pmkol/luci-app-timedreboot package/new/extd/luci-app-timedreboot
+
+# autotimeset
+git clone https://$github/sirpdboy/luci-app-autotimeset package/new/luci-app-autotimeset
